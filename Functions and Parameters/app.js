47501eadd61d62fc6530f1762463f14a6ca907da
evangel <evangeldaping@gmail.com>
2021-11-04 15:40:19 +0800

    toUpperCase and add functions

---
+++ Functions and Parameters/app.js
@@ -0,0 +1,16 @@
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+
+add(5, 10);
+
+const name = "Dev Ed";
+const me = "Evangel";
+
+function toUpper(text) {
+    const upperCased = text.toUpperCase();
+    console.log(upperCased);
+}
+
+toUpper(name);
+toUpper(me);
